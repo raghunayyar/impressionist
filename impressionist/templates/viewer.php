0e28bf8e5422f302a1387dbef5e2cfaadf47a64d
Raghu Nayyar <raghu.nayyar.007@gmail.com>
2012-09-26 22:18:54 +0530

    Update impressionist/templates/viewer.php
    style id same as the impress app. 

--- impressionist/templates/viewer.php
+++ impressionist/templates/viewer.php
@@ -1,7 +1,7 @@
 <?php
     
 //CHeck if this linktiabsolute is working fine.
-    echo ('<div id="top-navbar">
+    echo ('<div id="controls">
         
        <a href="'.\OCP\Util::linkToAbsolute('impressionist','js/main.php').'" class="btn">' .$l->t('Create Presentation').'</a>         
             </div>
@@ -12,11 +12,12 @@ if(empty($_['list'])) {
     echo('<div id="emptyfolder">'.$l->t('No Impress files were created using Impressionist in your ownCloud, please create a file first.').'</div>');
 } 
 else {
-	echo('<table class="impression-list" >');
+	//These files come from the impressionist way of saving things. Check. 
+	echo('<table class="impresslist">');
 	
         foreach($_['list'] as $entry) {
             
-		echo('<tr><td width="1"><a target="_blank" href="'.\OCP\Util::linkToAbsolute('impressionist','impressionist-run.php').'&file='.urlencode($entry['url']).'&name='.urlencode($entry['name']).'"><img align="left" src="'.\OCP\Util::linkToAbsolute('impressionist','img/rightarrow.png').'"></a></td><td><a target="_blank" href="'.\OCP\Util::linkToAbsolute('impressionist','impressionist-run.php').'&file='.urlencode($entry['url']).'&name='.urlencode($entry['name']).'">'.$entry['name'].'</a></td><td>'.\OCP\Util::formatDate($entry['mtime']).'</td><td>'.\OCP\Util::humanFileSize($entry['size']).'</td></tr>');
+		echo('<tr><td width="1"><a target="_blank" href="'.\OCP\Util::linkToAbsolute('impressionist','impressionist-run.php').'&file='.urlencode($entry['url']).'&name='.urlencode($entry['name']).'"><img align="left" src="'.\OCP\Util::linkToAbsolute('impressionist','img/impress.png').'"></a></td><td><a target="_blank" href="'.\OCP\Util::linkToAbsolute('impressionist','impressionist-run.php').'&file='.urlencode($entry['url']).'&name='.urlencode($entry['name']).'">'.$entry['name'].'</a></td><td>'.\OCP\Util::formatDate($entry['mtime']).'</td><td>'.\OCP\Util::humanFileSize($entry['size']).'</td></tr>');
 	        
         }
 	
